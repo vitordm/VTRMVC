81320ccf0830feb7c78334b3d9ad8fc326ea3a0e
Vitor G. Isse de Oliveira <oliveira.vitor3@gmail.com>
2015-10-16 10:24:34 -0300

    It's not changes, but I have to commit

--- Sample/CRUD/www/index.php
+++ Sample/CRUD/www/index.php
@@ -2,11 +2,10 @@
 
 include "../../../vendor/autoload.php";
 
-
 try {
-	$app = new VTRMVC\Core\App();
-	$url = isset($_GET['_url']) ? $_GET['_url'] : "";
-	$app->start("../start_sample.json", $url);
+	$app = new VTRMVC\Core\App(); //Create object of Framework
+	$url = isset($_GET['_url']) ? $_GET['_url'] : ""; //get the Friendly URL
+	$app->start("../start_sample.json", $url); //Start
 } catch (\VTRMVC\Core\Exceptions\InvalidConfigurationException $inC) {
 	\VTRMVC\Util\Util::varzx($inC->getMessage());
 } catch (Exception $ex) {
